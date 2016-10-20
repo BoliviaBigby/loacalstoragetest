709a4e66867c5b46447dcdd5e8e8a352e5f5af2e
BoliviaBigby <rigo1_15@uni.worc.ac.uk>
2016-10-20 15:39:41 +0100

    code update with js
    added js with html

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,10 @@
-   function onLoad() {
+
+    function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready"); //here is the event listener 
     }
 	
-
+	
 
     // device APIs are available
     // here is the event handler
@@ -11,3 +12,25 @@
 		alert("hi olivia"); //the event handler reacts to a function
         
 	
+    
+var key1 = "pen";
+var value1 = "green";
+
+var key2 = "botttle";
+var value2 = "water";
+            
+var key3 = "bag";
+var value3 = "leather";
+            
+var key4 = "pencil";
+var value4 ="lead";
+            
+var key5 = "phone";
+var value5 = "white";
+
+window.localStorage.setItem( key1, value1 );
+window.localStorage.setItem( key2, value2 );
+window.localStorage.setItem( key3, value3 );
+window.localStorage.setItem( key4, value4 );
+window.localStorage.setItem( key5, value5 );
+    }
